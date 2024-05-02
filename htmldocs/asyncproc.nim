5e1877586abf728acfc76b39c2b1d117cd8fbd0a
alogani <alogani@mailfence.com>
2024-05-02 08:49:55 +0200

    Update docs

--- htmldocs/asyncproc.nim
+++ htmldocs/asyncproc.nim
@@ -1,6 +1,5 @@
 import ../src/asyncproc
 import ../src/asyncproc/exports/asyncprocimpl
-import ../src/asyncproc/exports/procargs
+import ../src/asyncproc/exports/procargsresult
 import ../src/asyncproc/exports/procenv
 import ../src/asyncproc/exports/procmacro
-import ../src/asyncproc/exports/procresult
